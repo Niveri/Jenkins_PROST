d61f084bd35c9d4ebdc75a7362bc6f582d561996
ekurach <emiliakurach@o2.pl>
2020-05-13 13:34:41 +0200

    add substract

--- Jenkins_PROST.h
+++ Jenkins_PROST.h
@@ -16,12 +16,15 @@ auto multiply = [](const auto first, const auto second) {
 	return first * second;
 };
 
-
+auto substract = [](const auto first, const auto second) {
+	return first + second;
+};
 
 auto initializer = [](const auto first, const auto second) ->std::map<std::string, std::function<int()>> {
 	return {
 		{"add", std::bind(add, first, second)},
 		{"multiply", std::bind(multiply, first, second)},
+		{"substract", std::bind(substract, first, second)}
 		
 		
 	};
